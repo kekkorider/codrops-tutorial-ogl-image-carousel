275115f2122ec9c1cf05ac3ce218960115d59e23
Francesco Michelini <hello@francescomichelini.com>
2021-08-04 17:34:48 +0200

    Step 06 - Move triangle creation to a separate function - Move Grid creation to a separate function

--- src/shaders/effect.fragment.glsl
+++ src/shaders/effect.fragment.glsl
@@ -7,6 +7,28 @@ varying vec2 vUv;
 
 #pragma glslify: Shape = require(./modules/Shape)
 
+float Triangle(vec2 uv, vec2 position) {
+  float size = 0.2;
+  float sides = 3.0;
+  float blur = 0.001;
+
+  return Shape(uv, position, size, sides, blur);
+}
+
+float Tiles(vec2 uv) {
+  float result = 0.0;
+
+  // This makes the UVs repeat infinitely on both axes
+  // depending on how many times the UVs are multiplied,
+  // creating the illusion of a grid.
+  // "gv" stands for "grid UV".
+  vec2 gv = fract(uv*5.0);
+
+  result += Triangle(gv, vec2(0.5));
+
+  return result;
+}
+
 void main() {
   // UV coordinates that go from -1 to +1,
   // useful to easily align things at the center of the screen.
@@ -15,19 +37,7 @@ void main() {
 
   vec3 color = vec3(0.0);
 
-  // This makes the UVs repeat infinitely on both axes
-  // depending on how many times the UVs are multiplied,
-  // creating the illusion of a grid.
-  // "gv" stands for "grid UV".
-  vec2 gv = fract(uv*5.0);
-
-  vec2 pos = vec2(0.5);
-  float size = 0.2;
-  float sides = 3.0;
-  float blur = 0.001;
-  float triangle = Shape(gv, pos, size, sides, blur);
-
-  color += triangle;
+  color += Tiles(uv);
 
   // Debug axes
   float pixel = 1.0 / uResolution.x;
