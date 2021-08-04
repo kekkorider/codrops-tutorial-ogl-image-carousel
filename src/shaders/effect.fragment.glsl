9655879fd70b2cf1e762e3d2ec2c55a98c76145c
Francesco Michelini <hello@francescomichelini.com>
2021-08-04 16:21:27 +0200

    Step 05 - Generate a grid of triangles - Add debug axes

--- src/shaders/effect.fragment.glsl
+++ src/shaders/effect.fragment.glsl
@@ -15,13 +15,24 @@ void main() {
 
   vec3 color = vec3(0.0);
 
-  vec2 pos = vec2(0.0);
-  float size = 0.1;
+  // This makes the UVs repeat infinitely on both axes
+  // depending on how many times the UVs are multiplied,
+  // creating the illusion of a grid.
+  // "gv" stands for "grid UV".
+  vec2 gv = fract(uv*5.0);
+
+  vec2 pos = vec2(0.5);
+  float size = 0.2;
   float sides = 3.0;
   float blur = 0.001;
-  float triangle = Shape(uv, pos, size, sides, blur);
+  float triangle = Shape(gv, pos, size, sides, blur);
 
   color += triangle;
+
+  // Debug axes
+  float pixel = 1.0 / uResolution.x;
+  color.r += 1.0 - step(pixel*4.0, abs(uv.x));
+  color.r += 1.0 - step(pixel*4.0, abs(uv.y));
 
   gl_FragColor = vec4(color, 1.0);
 }
